85b44a2a30dc01164361dace51478501fd7282d1
Rima Alrwais <ralrwais@ralrwais-ltm2.internal.salesforce.com>
2017-02-17 16:24:39 -0800

    updates

---
+++ assets/javascript/game.js
@@ -0,0 +1,52 @@
+var number = [];
+
+	for (var i=0; i<number.length; i++){
+		var random = $("<div>")
+
+		random.addClass("box3");}
+
+		var number =
+		$("box3").on("click", function() {
+			var number = Math.floor(Math.random() * 120) + 19;
+			$(".box3").html(random);
+
+		})
+
+
+var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"]; 
+
+ 	for (var i=0; i<numbers.length; i++)
+ 	{
+ 		var buttons = $("<button>");
+
+ 		buttons.addClass("gem1 gem2 gem3 gem4");}
+
+ 		var buttons =
+ 		$(".gem1").on("click", function() {
+ 			var buttons = Math.floor(Math.random() * 12) + 1;
+ 			$(".gem1").html(buttons); 
+
+ 		})
+
+ 		var buttons =
+ 		$(".gem2").on("click", function() {
+ 			var buttons = Math.floor(Math.random() * 12) + 1;
+ 			$(".gem2").html(buttons); 
+
+ 		})
+
+ 		var buttons =
+ 		$(".gem3").on("click", function() {
+ 			var buttons = Math.floor(Math.random() * 12) + 1;
+ 			$(".gem3").html(buttons); 
+
+ 		})
+
+
+ 		var buttons =
+ 		$(".gem4").on("click", function() {
+ 			var buttons = Math.floor(Math.random() * 12) + 1;
+ 			$(".gem4").html(buttons); 
+
+ 		})
+
